ce7616bdcfddd601761aa66bcdc76ecc640acba6
charlesunknown <nelzy@et.esiea.fr>
2025-11-07 14:56:54 +0200

    invisible

--- task-h/index.js
+++ task-h/index.js
@@ -164,7 +164,7 @@ document.addEventListener("DOMContentLoaded", () => {
       });
       const Timestamp =  now.toLocaleTimeString("en-GB");
       TimestampCell.textContent = formattedDate +" at "+ Timestamp;
-      TimestampCell.className = "text-center max-lg:text-[2rem]";
+      TimestampCell.className = "max-lg:invisible text-center max-lg:text-[2rem]";
 
 
       row.appendChild(TimestampCell);
