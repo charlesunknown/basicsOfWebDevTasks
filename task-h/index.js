ebee0d633f5b52ac1662655a42abbc06a41b276d
charlesunknown <nelzy@et.esiea.fr>
2025-11-07 15:00:42 +0200

    widtglo

--- task-h/index.js
+++ task-h/index.js
@@ -164,7 +164,7 @@ document.addEventListener("DOMContentLoaded", () => {
       });
       const Timestamp =  now.toLocaleTimeString("en-GB");
       TimestampCell.textContent = formattedDate +" at "+ Timestamp;
-      TimestampCell.className = "max-lg:invisible text-center max-lg:text-[2rem]";
+      TimestampCell.className = "max-lg:hidden text-center max-lg:text-[2rem]";
 
 
       row.appendChild(TimestampCell);
