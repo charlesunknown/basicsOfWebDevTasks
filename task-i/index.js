960c74b12f903ca68db77ef738fb6d3c94d05331
charlesunknown <nelzy@et.esiea.fr>
2025-11-20 17:52:57 +0200

    newstuff

--- task-i/index.js
+++ task-i/index.js
@@ -5,21 +5,21 @@
 
 function showOfferDialog(hisName, stats, img) {
     let cdialog = document.createElement("dialog");
+    const title_div = document.createElement("div");
+
+    title_div.id= "title_div";
     const Name_dialog = document.createElement("h1");
+    Name_dialog.id= "dialog_title";
     Name_dialog.innerHTML = hisName;
 
-    const text_div = document.createElement("div");
+    const button_close = document.createElement("button");
+    button_close.onclick = () => {cdialog.close(); cdialog.remove()};
+    button_close.id ="close";
+    button_close.innerHTML="&#10006"; 
+    title_div.appendChild(button_close);
 
-    const Stats_dialog = document.createElement("h2");
-    text_div.id ="dialog_text"
-    Stats_dialog.innerHTML = stats;
-
-    cdialog.appendChild(Name_dialog);
-
-    text_div.appendChild(Stats_dialog);
-
-    cdialog.appendChild(text_div);
-
+    title_div.appendChild(Name_dialog);
+    cdialog.appendChild(title_div);
     const img_div = document.createElement("div");
     const div_img= document.createElement("img");
     div_img.src=img;
@@ -29,11 +29,20 @@ function showOfferDialog(hisName, stats, img) {
 
     cdialog.appendChild(img_div);
 
-    const button_close = document.createElement("button");
-    button_close.onclick = () => {cdialog.close(); cdialog.remove()};
-    button_close.id ="close";
-    button_close.innerHTML="&#10006"; 
-    cdialog.appendChild(button_close);
+    const text_div = document.createElement("div");
+
+    const Stats_dialog = document.createElement("h1");
+    text_div.id ="dialog_text"
+    Stats_dialog.innerHTML = stats;
+
+
+    text_div.appendChild(Stats_dialog);
+
+    cdialog.appendChild(text_div);
+    
+    const price_div = document.createElement("div");
+    
+
 
     const section_dialog=document.querySelector(".showcase-products");
 
