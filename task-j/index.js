2a4a3fd8fefcf104c783b6de1c80ff79f1b862ca
charlesunknown <nelzy@et.esiea.fr>
2025-11-21 14:14:38 +0200

    task-j

---
+++ task-j/index.js
@@ -0,0 +1,57 @@
+
+// index.js
+
+
+
+function showOfferDialog(hisName, stats, img, price) {
+    let cdialog = document.createElement("dialog");
+    const title_div = document.createElement("div");
+
+    title_div.id= "title_div";
+    const Name_dialog = document.createElement("h1");
+    Name_dialog.id= "dialog_title";
+    Name_dialog.innerHTML = hisName;
+
+    const button_close = document.createElement("button");
+    button_close.onclick = () => {cdialog.close(); cdialog.remove()};
+    button_close.id ="close";
+    button_close.innerHTML="&#10006"; 
+    title_div.appendChild(button_close);
+
+    title_div.appendChild(Name_dialog);
+    cdialog.appendChild(title_div);
+    const img_div = document.createElement("div");
+    const div_img= document.createElement("img");
+    div_img.alt= img.alt;
+    div_img.src=img;
+    img_div.id="dialog_img";
+    
+    img_div.appendChild(div_img);
+
+    cdialog.appendChild(img_div);
+
+    const text_div = document.createElement("div");
+
+    const Stats_dialog = document.createElement("h2");
+    text_div.id ="dialog_text";
+    Stats_dialog.innerHTML = stats;
+    const price_dialog= document.createElement("h3");
+    price_dialog.innerHTML = price;
+
+
+    text_div.appendChild(Stats_dialog);
+    text_div.appendChild(price_dialog);
+
+    cdialog.appendChild(text_div);
+
+    
+
+
+    const section_dialog=document.querySelector(".showcase-products");
+
+    section_dialog.appendChild(cdialog);
+
+    cdialog.showModal();
+}
+
+
